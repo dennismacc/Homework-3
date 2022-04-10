4696eb16831042e71ef4163028c6c48b929bc8ef
dennismacc <dennis.maccarthy@gmail.com>
2022-04-09 17:34:26 -0700

    generate password function: prompt user to select # of characters and write if statements to provide conditions

--- Develop/script.js
+++ Develop/script.js
@@ -1,12 +1,23 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var numbers = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
-var upperCase = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
-var lowerCase = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-var symbols = ['!', '@', '#', '$'];  
+var numbers = "0123456789";
+var lowerCase = "abcdefghijklmnopqrstuvwxyz";
+var upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+var symbols = "!@#$%^&*";  
 
-function generatePassword(){
-  var userchoice = window.prompt("")
+function generatePassword() {
+ 
+  var passwordLenghtUser = prompt("How many characters would you like to have in your password? Choose a number between 8 and 128.")
+    if (passwordLenghtUser < 8) {
+      alert("Password must contain at least 8 characters")
+      return '';
+    } 
+    if (passwordLenghtUser > 128) {
+      alert("Password cannot contain more than 128 characters")
+      return '';
+    }
+
+
 }
 
 // Write password to the #password input
