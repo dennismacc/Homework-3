45be153fb56bca7bf818f4f2df577ceea8fbc4c4
dennismacc <dennis.maccarthy@gmail.com>
2022-04-11 19:45:19 -0700

    add return to generate password function. update for loop with += to append password

--- Develop/script.js
+++ Develop/script.js
@@ -5,10 +5,11 @@ var lowerCase = ["abcdefghijklmnopqrstuvwxyz"];
 var upperCase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
 var symbols = ["!@#$%^&*"];  
 
-var password = "";
-var passwordText = "";
+
 
 function generatePassword() {
+  var password = "";
+  var passwordText = "";
  // Prompt user to select how many characters they would like in their password and tells them if they're choice does not meet criteria
   var passwordLength = prompt("How many characters would you like to have in your password? Choose a number between 8 and 128.")
     if (passwordLength < 8) {
@@ -20,29 +21,30 @@ function generatePassword() {
       return '';
     }
 // Ask user if they would like to include lowercase letters
-  var passwordLowerCaseChoice = confirm("Would you like to include lowercase letters in your password?")
+  var passwordLowerCaseChoice = confirm("Would you like to include lowercase letters in your password?");
     if (passwordLowerCaseChoice) {
       passwordText += lowerCase;
-    }
+    } 
 // Ask user if they would like to include uppercase letters
-    var passwordUpperCaseChoice = confirm("Would you like to include uppercase letters in your password?")
+    var passwordUpperCaseChoice = confirm("Would you like to include uppercase letters in your password?");
       if (passwordUpperCaseChoice) {
         passwordText += upperCase;
       }
 // Ask user if they would like to include numbers
-    var passwordNumberChoice = confirm("Would you like to include numbers in your password?")
+    var passwordNumberChoice = confirm("Would you like to include numbers in your password?");
       if (passwordNumberChoice) {
         passwordText += numbers;
       }
 // Ask user if they would like to include symbols 
-    var passwordSymbolChoice = confirm("Would you like to include symbols in your password?")
+    var passwordSymbolChoice = confirm("Would you like to include symbols in your password?");
       if (passwordSymbolChoice) {
         passwordText += symbols;
       }
-
   for (let i = 0; i < passwordLength; i++) {
-    password = passwordText(Math.floor(Math.random()*passwordText.length))
+    password += passwordText[Math.floor(Math.random()*passwordText.length)]
   }
+
+  return password;
 }
 
 // Write password to the #password input
